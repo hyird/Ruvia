9f4855b7c53bc9b20ff6dff63e7a6409395c0ecb
hyird <hyird@outlook.com>
2026-06-27 08:05:38 +0800

    Split HTTP2 session result types

--- src/net/http2/Http2ServerSession.h
+++ src/net/http2/Http2ServerSession.h
@@ -45,6 +45,7 @@
 #include "Http2ResponseHeaders.h"
 #include "Http2ResponseStreamSink.h"
 #include "Http2SessionAwaiters.h"
+#include "Http2SessionResults.h"
 #include "Http2StreamTable.h"
 #include "Http2WebSocket.h"
 #include "Http2WebSocketHandshake.h"
@@ -74,135 +75,6 @@
 #include "ruvia/memory/MemoryPool.h"
 
 namespace ruvia::detail {
-
-class Http2RouteDispatchResult final {
-public:
-    [[nodiscard]] static Http2RouteDispatchResult makeStreamHandled() noexcept {
-        return Http2RouteDispatchResult(Outcome::kStreamHandled);
-    }
-
-    [[nodiscard]] static Http2RouteDispatchResult makeBufferedResponse(HttpResponse response) {
-        return Http2RouteDispatchResult(std::move(response));
-    }
-
-    [[nodiscard]] bool streamHandled() const noexcept {
-        return outcome_ == Outcome::kStreamHandled;
-    }
-
-    [[nodiscard]] bool bufferedResponse() const noexcept {
-        return outcome_ == Outcome::kBufferedResponse;
-    }
-
-    [[nodiscard]] HttpResponse takeResponse() noexcept {
-        return std::move(*response_);
-    }
-
-private:
-    enum class Outcome {
-        kStreamHandled,
-        kBufferedResponse,
-    };
-
-    explicit Http2RouteDispatchResult(Outcome outcome) noexcept
-        : outcome_(outcome) {}
-
-    explicit Http2RouteDispatchResult(HttpResponse response)
-        : outcome_(Outcome::kBufferedResponse),
-          response_(std::move(response)) {}
-
-    Outcome outcome_;
-    std::optional<HttpResponse> response_;
-};
-
-class Http2SessionFlow final {
-public:
-    [[nodiscard]] static constexpr Http2SessionFlow keepRunning() noexcept {
-        return Http2SessionFlow(State::kKeepRunning);
-    }
-
-    [[nodiscard]] static constexpr Http2SessionFlow stopRunning() noexcept {
-        return Http2SessionFlow(State::kStopRunning);
-    }
-
-    [[nodiscard]] constexpr bool shouldContinue() const noexcept {
-        return state_ == State::kKeepRunning;
-    }
-
-    [[nodiscard]] constexpr bool shouldStop() const noexcept {
-        return state_ == State::kStopRunning;
-    }
-
-private:
-    enum class State : std::uint8_t {
-        kKeepRunning,
-        kStopRunning,
-    };
-
-    constexpr explicit Http2SessionFlow(State state) noexcept
-        : state_(state) {}
-
-    State state_;
-};
-
-class Http2InputReadResult final {
-public:
-    [[nodiscard]] static constexpr Http2InputReadResult ready() noexcept {
-        return Http2InputReadResult(State::kReady);
-    }
-
-    [[nodiscard]] static constexpr Http2InputReadResult stopReading() noexcept {
-        return Http2InputReadResult(State::kStopReading);
-    }
-
-    [[nodiscard]] constexpr bool isReady() const noexcept {
-        return state_ == State::kReady;
-    }
-
-    [[nodiscard]] constexpr bool shouldStop() const noexcept {
-        return state_ == State::kStopReading;
-    }
-
-private:
-    enum class State : std::uint8_t {
-        kReady,
-        kStopReading,
-    };
-
-    constexpr explicit Http2InputReadResult(State state) noexcept
-        : state_(state) {}
-
-    State state_;
-};
-
-class Http2DataWindowResult final {
-public:
-    [[nodiscard]] static constexpr Http2DataWindowResult ready() noexcept {
-        return Http2DataWindowResult(State::kReady);
-    }
-
-    [[nodiscard]] static constexpr Http2DataWindowResult stopWriting() noexcept {
-        return Http2DataWindowResult(State::kStopWriting);
-    }
-
-    [[nodiscard]] constexpr bool isReady() const noexcept {
-        return state_ == State::kReady;
-    }
-
-    [[nodiscard]] constexpr bool shouldStop() const noexcept {
-        return state_ == State::kStopWriting;
-    }
-
-private:
-    enum class State : std::uint8_t {
-        kReady,
-        kStopWriting,
-    };
-
-    constexpr explicit Http2DataWindowResult(State state) noexcept
-        : state_(state) {}
-
-    State state_;
-};
 
 template <typename Stream>
 class Http2ServerSession final {
