58bb92ada63455715309ad72719af23245089ee5
hyird <hyird@outlook.com>
2026-06-30 16:06:22 +0800

    Simplify next continuation state

--- src/router/core/Router.cpp
+++ src/router/core/Router.cpp
@@ -10,7 +10,7 @@ namespace ruvia {
 using namespace detail;
 
 Task<void> Next::operator()() const {
-    return callable_(*context_);
+    return callable_();
 }
 
 detail::RouterImpl::RouterImpl(Router& router) noexcept
