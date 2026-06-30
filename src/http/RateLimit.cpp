ea82fb6361d9b15ba4c2992d79c2d320344a5011
hyird <hyird@outlook.com>
2026-06-30 15:52:41 +0800

    Move request metadata access onto request

--- src/http/RateLimit.cpp
+++ src/http/RateLimit.cpp
@@ -15,7 +15,7 @@ RouteRateLimitResult checkRouteRateLimit(
 
     const auto check = limiter->allowRoute(
         ContextAccess::routeRateLimitScope(context),
-        context.remoteAddress(),
+        context.req().remoteAddress(),
         options.rule);
     return RouteRateLimitResult{
         .allowed = check.allowed,
