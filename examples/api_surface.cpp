5c229f311246e417c8372d762003b44c21a13179
hyird <hyird@outlook.com>
2026-07-02 04:22:44 +0800

    Remove request clone member alias

--- examples/api_surface.cpp
+++ examples/api_surface.cpp
@@ -68,6 +68,11 @@ concept HasRequestBytesAlias = requires(const T& request) {
     request.bytes();
 };
 
+template <typename T>
+concept HasMemberCloneRawRequestAlias = requires(const T& request) {
+    request.cloneRawRequest();
+};
+
 static_assert(!std::is_copy_constructible_v<ruvia::Next>);
 static_assert(!std::is_copy_assignable_v<ruvia::Next>);
 static_assert(!std::is_move_constructible_v<ruvia::Next>);
@@ -101,6 +106,7 @@ static_assert(!HasUnaryContextCookie<ruvia::Context>);
 static_assert(!HasUnaryContextParam<ruvia::Context>);
 static_assert(!HasResponseHeadersAlias<ruvia::HttpResponse>);
 static_assert(!HasRequestBytesAlias<ruvia::ContextRequest>);
+static_assert(!HasMemberCloneRawRequestAlias<ruvia::ContextRequest>);
 static_assert(std::is_same_v<
     decltype(std::declval<const ruvia::ContextRequest&>().cookie()),
     const ruvia::RequestNameValueList&>);
