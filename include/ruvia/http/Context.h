a23d2a7b74b41a241f48912518307e8408074ca6
hyird <hyird@outlook.com>
2026-07-02 06:25:43 +0800

    Align parseBody single value selection

--- include/ruvia/http/Context.h
+++ include/ruvia/http/Context.h
@@ -348,7 +348,7 @@ public:
             }
 
             [[nodiscard]] const RequestFormField* field() const noexcept {
-                return fields_.empty() ? nullptr : fields_.front();
+                return fields_.empty() ? nullptr : fields_.back();
             }
 
             [[nodiscard]] std::span<const RequestFormField* const> fields() const noexcept {
@@ -529,7 +529,7 @@ public:
             }
 
             [[nodiscard]] const RequestFormField* field() const noexcept {
-                return fields_.empty() ? nullptr : fields_.front();
+                return fields_.empty() ? nullptr : fields_.back();
             }
 
             [[nodiscard]] const RequestFormField* operator->() const noexcept {
