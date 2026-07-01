01e16a052612aedc04e18fa27dc3ab9dfd95073d
hyird <hyird@outlook.com>
2026-07-01 16:58:59 +0800

    Add Hono-style validation target names

--- include/ruvia/http/ValidationTypes.h
+++ include/ruvia/http/ValidationTypes.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+#include <stdexcept>
+#include <string_view>
 
 namespace ruvia {
 
@@ -19,5 +21,27 @@ inline constexpr ValidationTarget Query = ValidationTarget::kQuery;
 inline constexpr ValidationTarget Param = ValidationTarget::kParam;
 inline constexpr ValidationTarget Header = ValidationTarget::kHeader;
 inline constexpr ValidationTarget Cookie = ValidationTarget::kCookie;
+
+[[nodiscard]] inline ValidationTarget validationTargetFromName(std::string_view name) {
+    if (name == "json") {
+        return ValidationTarget::kJson;
+    }
+    if (name == "form") {
+        return ValidationTarget::kForm;
+    }
+    if (name == "query") {
+        return ValidationTarget::kQuery;
+    }
+    if (name == "param") {
+        return ValidationTarget::kParam;
+    }
+    if (name == "header") {
+        return ValidationTarget::kHeader;
+    }
+    if (name == "cookie") {
+        return ValidationTarget::kCookie;
+    }
+    throw std::invalid_argument("unknown validation target");
+}
 
 }  // namespace ruvia
