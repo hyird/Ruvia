3354516e5509e8ff69184fdc62549cdba3aa058a
hyird <hyird@outlook.com>
2026-07-02 03:52:20 +0800

    Harden middleware response handling

--- examples/middleware_next.cpp
+++ examples/middleware_next.cpp
@@ -11,6 +11,28 @@ static_assert(!std::is_copy_assignable_v<ruvia::Next::Awaitable>);
 static_assert(!std::is_move_constructible_v<ruvia::Next::Awaitable>);
 static_assert(!std::is_move_assignable_v<ruvia::Next::Awaitable>);
 
+class ByValueNextMiddleware final : public ruvia::Middleware<ByValueNextMiddleware> {
+public:
+    ruvia::Task<void> handle(ruvia::Context&, ruvia::Next);
+};
+
+class ConstNextMiddleware final : public ruvia::Middleware<ConstNextMiddleware> {
+public:
+    ruvia::Task<void> handle(ruvia::Context&, const ruvia::Next&);
+};
+
+class RvalueNextMiddleware final : public ruvia::Middleware<RvalueNextMiddleware> {
+public:
+    ruvia::Task<void> handle(ruvia::Context&, ruvia::Next&&);
+};
+
+static_assert(!ruvia::detail::VoidHandleMiddleware<ByValueNextMiddleware>);
+static_assert(!ruvia::detail::VoidHandleMiddleware<ConstNextMiddleware>);
+static_assert(!ruvia::detail::VoidHandleMiddleware<RvalueNextMiddleware>);
+static_assert(!ruvia::detail::ResponseHandleMiddleware<ByValueNextMiddleware>);
+static_assert(!ruvia::detail::ResponseHandleMiddleware<ConstNextMiddleware>);
+static_assert(!ruvia::detail::ResponseHandleMiddleware<RvalueNextMiddleware>);
+
 class BorrowedNextMiddleware final : public ruvia::Middleware<BorrowedNextMiddleware> {
 public:
     ruvia::Task<void> handle(ruvia::Context&, ruvia::Next& next) {
