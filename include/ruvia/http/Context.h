ec020b1c68d0bcaaf2163385804c740cd803ee40
hyird <hyird@outlook.com>
2026-07-01 10:49:34 +0800

    Avoid default render options argument

--- include/ruvia/http/Context.h
+++ include/ruvia/http/Context.h
@@ -597,7 +597,9 @@ public:
 
     Context& setRenderer(Renderer renderer) noexcept;
 
-    [[nodiscard]] Task<HttpResponse> render(std::string_view body, RenderOptions options = RenderOptions{});
+    [[nodiscard]] Task<HttpResponse> render(std::string_view body);
+
+    [[nodiscard]] Task<HttpResponse> render(std::string_view body, RenderOptions options);
 
     [[nodiscard]] HttpResponse redirect(
         std::string_view location,
